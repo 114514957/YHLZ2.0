b1a2d5b81bc122a1e73492773aeb960431b79142
user <w20050808z@qq.com>
2026-09-09 21:32:53 +0800

    pet: 窗口自适应收缩贴合角色(去大透明框); resize居中

--- desktop/main.js
+++ desktop/main.js
@@ -39,6 +39,13 @@ function makeWindow() {
 ipcMain.on("pet-mouse", (_e, on) => {
   if (win) win.setIgnoreMouseEvents(!!on, { forward: true });
 });
+ipcMain.on("pet-size", (_e, { w, h }) => {
+  if (win) {
+    const ww = Math.max(90, Math.min(1600, Math.round(w)));
+    const hh = Math.max(90, Math.min(1600, Math.round(h)));
+    win.setContentSize(ww, hh, true);
+  }
+});
 
 function makeTray() {
   let icon = path.join(__dirname, "..", "assets", "icons", "yhlz-app2.ico");
--- desktop/preload.js
+++ desktop/preload.js
@@ -1,4 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("pet", {
   mouseThrough: (on) => ipcRenderer.send("pet-mouse", !!on),
+  setSize: (w, h) => ipcRenderer.send("pet-size", { w, h }),
 });
